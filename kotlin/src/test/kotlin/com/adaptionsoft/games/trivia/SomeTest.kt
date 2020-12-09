c4109b675a40b09404cd08182ee70e1f4b33230b
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2020-12-09 22:03:47 +0100

    Upgrade JUnit by about 6 years

--- kotlin/src/test/kotlin/com/adaptionsoft/games/trivia/SomeTest.kt
+++ kotlin/src/test/kotlin/com/adaptionsoft/games/trivia/SomeTest.kt
@@ -1,7 +1,8 @@
 package com.adaptionsoft.games.trivia
 
-import org.junit.Assert.assertTrue
-import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
 
 class SomeTest {
 
